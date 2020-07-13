ef515a54284f8f3ef8b7db54beaa2effb1bffd6f
car12o <car12o@users.noreply.github.com>
2020-07-13 17:47:27 +0100

    update

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-    commonjs: true,
-    es6: true
-  },
-  extends: [
-    "airbnb-base"
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parserOptions: {
-    ecmaVersion: 2018
-  },
   rules: {
     "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": true, "caughtErrors": "all" }],
     "no-console": 1,
